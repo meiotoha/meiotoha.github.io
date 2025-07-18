e6bf1fe24c6f701c2c00551b47bbac0fbff68004
maplelolita <maplelolita@gmail.com>
2025-07-18 16:47:22 +0800

    update

--- src/utils/i18n.ts
+++
@@ -1,9 +0,0 @@
-import type Strings from '../i18n/strings'
-import { enUs } from '../i18n/en-us'
-
-export function GetSysLang(langstr: string): Strings {
-  switch (langstr.toLocaleLowerCase()) {
-    default:
-      return enUs // 默认使用英文
-  }
-}
